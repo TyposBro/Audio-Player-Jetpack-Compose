b1077cd1c9b0b23836287ec8a78caf34b013b5eb
typosbro <typosbro@proton.me>
2024-07-07 20:21:16 +0900

    Service Module for DI

--- app/src/main/java/hoods/com/audioplayer/media/service/MediaPlayerService.kt
+++ app/src/main/java/hoods/com/audioplayer/media/service/MediaPlayerService.kt
@@ -3,9 +3,23 @@ package hoods.com.audioplayer.media.service
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import dagger.hilt.android.AndroidEntryPoint
 import hoods.com.audioplayer.media.constants.K
+import javax.inject.Inject
 
+
+//@AndroidEntryPoint
 class MediaPlayerService: MediaBrowserServiceCompat() {
+
+    @Inject
+    lateinit var dataSourceFactory: CacheDataSource.Factory
+
+    @Inject
+    lateinit var exoPlayer: ExoPlayer
+
+
     override fun onGetRoot(
         clientPackageName: String,
         clientUid: Int,
